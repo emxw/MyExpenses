fd38a68f6825d6fc08057ec5388eea2688fd3575
emxw <84562923+emxw@users.noreply.github.com>
2022-10-28 21:13:41 +0800

    Add charts feature

---
+++ app/src/main/java/com/example/myexpenses/ChartsFragment.java
@@ -0,0 +1,77 @@
+package com.example.myexpenses;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class ChartsFragment extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charts_fragment);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Daily"));
+        tabLayout.addTab(tabLayout.newTab().setText("Weekly"));
+        tabLayout.addTab(tabLayout.newTab().setText("Monthly"));
+        tabLayout.addTab(tabLayout.newTab().setText("Yearly"));
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                switch (position){
+                    case 0:
+                        Daily daily = new Daily();
+                        return daily;
+                    case 1:
+                        Weekly weekly = new Weekly();
+                        return weekly;
+                    case 2:
+                        Monthly monthly = new Monthly();
+                        return monthly;
+                    case 3:
+                        Yearly yearly = new Yearly();
+                        return yearly;
+                    default:
+                        return null;
+
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return tabLayout.getTabCount();
+            }
+        });
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
